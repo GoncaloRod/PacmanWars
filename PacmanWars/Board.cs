8986d99e6d63167044549d18ad8586f135c45448
Gonçalo Rodrigues <rodriguesgonalo604@gmail.com>
2020-03-13 18:42:36 +0000

    Implemented basic code for board drawing

---
+++ PacmanWars/Board.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PacmanWars
+{
+    public class Board : DrawableGameComponent
+    {
+        private Game1 _game;
+        private SpriteBatch _batch;
+
+        private Texture2D _spriteSheet;
+        private int _width, _heigth;
+        private char[,] _matrix;
+
+        public Board(Game1 game, int width, int height, char[,] matrix) : base(game)
+        {
+            _game = game;
+            _batch = game.SpriteBatch;
+
+            _spriteSheet = game.SpriteSheet;
+            _width = width;
+            _heigth = height;
+            _matrix = matrix;
+        }
+
+        public char this[int x, int y] => _matrix[x, y];
+
+        public override void Draw(GameTime gameTime)
+        {
+            _batch.Begin();
+
+            for (int x = 0; x < _width; x++)
+            {
+                for (int y = 0; y < _heigth; y++)
+                {
+                    Rectangle outRectangle = new Rectangle(new Point(x  * Game1.TileSize, y * Game1.TileSize), new Point(Game1.TileSize));
+
+                    if (_matrix[x, y] == 'W')
+                    {
+                        _batch.Draw(
+                            texture: _spriteSheet,
+                            destinationRectangle: outRectangle,
+                            sourceRectangle: new Rectangle(0, 13 * 16, 16, 16),
+                            color: Color.White
+                        );
+                    }
+                }
+            }
+
+            _batch.End();
+        }
+    }
+}
--- PacmanWars/Game1.cs
+++ PacmanWars/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.IO;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,14 +10,23 @@ namespace PacmanWars
     /// </summary>
     public class Game1 : Game
     {
+        public static int TileSize = 32;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        
+
+        private Texture2D _spriteSheet;
+        private Board _board;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
+
+        public SpriteBatch SpriteBatch => _spriteBatch;
+        public Texture2D SpriteSheet => _spriteSheet;
+        public Board Board => _board;
 
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
@@ -40,7 +50,9 @@ namespace PacmanWars
             // Create a new SpriteBatch, which can be used to draw textures.
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            _spriteSheet = Content.Load<Texture2D>("ss");
+
+            LoadLevel();
         }
 
         /// <summary>
@@ -73,11 +85,65 @@ namespace PacmanWars
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+        private void LoadLevel()
+        {
+            string[] file = File.ReadAllLines($@"{Content.RootDirectory}\board.txt");
+
+            int width = file[0].Length;
+            int height = file.Length;
+            char[,] boardMatrix = new char[width, height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // Wall             'W'
+                    // "Pac-Dot"        'D'
+                    // "Power Pellet"   'P'
+                    // Player 1         '1'
+                    // Player 2         '2'
+                    // Enemy Spawn      'S'
+
+                    switch (file[y][x])
+                    {
+                        case 'W':
+                            boardMatrix[x, y] = 'W';
+                            break;
+                        case 'D':
+                            boardMatrix[x, y] = ' ';
+                            break;
+                        case 'P':
+                            boardMatrix[x, y] = ' ';
+                            break;
+                        case '1':
+                            boardMatrix[x, y] = ' ';
+                            break;
+                        case '2':
+                            boardMatrix[x, y] = ' ';
+                            break;
+                        case 'S':
+                            boardMatrix[x, y] = ' ';
+                            break;
+                        default:
+                            boardMatrix[x, y] = ' ';
+                            break;
+                    }
+                }
+            }
+
+            _board = new Board(this, width, height, boardMatrix);
+            Components.Add(_board);
+
+            _graphics.PreferredBackBufferWidth = width * TileSize;
+            _graphics.PreferredBackBufferHeight = (height + 1) * TileSize;
+            _graphics.ApplyChanges();
         }
     }
 }
