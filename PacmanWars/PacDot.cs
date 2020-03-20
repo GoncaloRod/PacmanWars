e3adc73de445a947f9e7cb01aa889927fc5ad6b9
Diogo Figueiredo <daof2001@gmail.com>
2020-03-20 13:43:04 +0000

    Supper Buggy PacDot Update(); To fix later

--- PacmanWars/PacDot.cs
+++ PacmanWars/PacDot.cs
@@ -27,7 +27,54 @@ namespace PacmanWars
 
         public override void Update(GameTime gameTime)
         {
-            
+            //Variables
+            Rectangle player1Area;
+            Rectangle player2Area;
+            float player1DistanceToDot, player2DistanceToDot;
+
+            //Players position
+            player1Area = new Rectangle((_game.Player1.Position).ToPoint(), new Point(Game1.TileSize));
+            player2Area = new Rectangle((_game.Player2.Position).ToPoint(), new Point(Game1.TileSize));
+           
+            Rectangle pacDotArea = new Rectangle((_position).ToPoint(), new Point(Game1.TileSize / 2));
+
+            //Special Case: if player 1 and player 2 intersect pacdot position at the exact same time
+            if (pacDotArea.Intersects(player1Area) && pacDotArea.Intersects(player2Area))
+            {
+                player1DistanceToDot = Vector2.Distance(_game.Player1.Position, _position);
+                player2DistanceToDot = Vector2.Distance(_game.Player2.Position, _position);
+    
+                if (player1DistanceToDot < player2DistanceToDot)
+                    _game.Player1.AddPoints(10);
+
+                if (player2DistanceToDot < player1DistanceToDot)
+	                _game.Player2.AddPoints(10);
+
+                //If the distance is equal, attributte points to the lowest scored player
+                else if (_game.Player1.Score < _game.Player2.Score && player1DistanceToDot == player2DistanceToDot)
+                        _game.Player1.AddPoints(10);
+                else
+                    _game.Player2.AddPoints(10); 
+
+                _game.PacDots.Remove(this);
+                _game.Components.Remove(this);
+            }
+
+            if (pacDotArea.Intersects(player1Area))
+	        {
+                _game.Player1.AddPoints(10);
+                _game.PacDots.Remove(this);
+                _game.Components.Remove(this);
+	        }
+
+            if (pacDotArea.Intersects(player2Area))
+	        {
+                _game.Player2.AddPoints(10);
+                _game.PacDots.Remove(this);
+                _game.Components.Remove(this);
+	        }
+                
+	        
         }
 
         public override void Draw(GameTime gameTime)
