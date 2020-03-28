996de7225c8aaaa6ffe3ebba76177bf7c26fab72
Diogo Figueiredo <daof2001@gmail.com>
2020-03-28 15:58:35 +0000

    Finished Update; pick up Sound also implemented

--- PacmanWars/Fruit.cs
+++ PacmanWars/Fruit.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
 
 namespace PacmanWars
 {
@@ -20,9 +21,14 @@ namespace PacmanWars
         private int _type;
         private int _score;
         private Dictionary<int, Vector2> _fruitsType;
+        private SoundEffect _pickUpSound;
+
+        private bool _destroyNextFrame = false;
 
         public Fruit(Game1 game,Point position, int type) : base(game)
         {
+            DrawOrder = 0;
+
             _game = game;
             _batch = game.SpriteBatch;
             _spriteSheet = game.SpriteSheet;
@@ -58,12 +64,41 @@ namespace PacmanWars
                 default:
                     break;
             }
+
+            _pickUpSound = game.Content.Load<SoundEffect>("Pickup");
+        }
+
+        protected override void UnloadContent()
+        {
+            _pickUpSound.Dispose();
+
+            base.UnloadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
             Rectangle fruitArea = new Rectangle(_position.Multiply(Game1.TileSize) + new Point((Game1.TileSize - _size) / 2), new Point(_size));
 
+            if (_destroyNextFrame)
+            {
+                _game.Fruits.Remove(this);
+                _game.Components.Remove(this);
+
+                return;
+            }
+
+            if (fruitArea.Intersects(_game.Player1.Area))
+            {
+                _destroyNextFrame = true;
+
+                _game.Player1.AddPoints(_score);
+            }
+            else if (fruitArea.Intersects(_game.Player1.Area))
+            {
+                _destroyNextFrame = true;
+
+                _game.Player2.AddPoints(_score);
+            }
         }
 
         public override void Draw(GameTime gameTime)
@@ -76,6 +111,11 @@ namespace PacmanWars
                 sourceRectangle: new Rectangle((_fruitsType[_type] * 16).ToPoint(), new Point(1,1).Multiply(16)),
                 color: Color.White
                 );
+
+            if (_destroyNextFrame)
+            {
+                _pickUpSound.Play();
+            }
 
             _batch.End();
         }
